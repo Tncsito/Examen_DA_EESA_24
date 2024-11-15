282cccd7bdf936e2c556748e2e2f9a68edbdcf76
Tncsito <eduarosantoyo@hotmail.com>
2024-11-15 13:57:20 -0700

    Agregar archivos de proyecto.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int Uno = Convert.ToInt32(uno.Text);
+            int Dos = Convert.ToInt32(dos.Text);
+            int r = Convert.ToInt32(R.Text);
+            if (suma.IsChecked)
+            {
+                r = Uno + Dos;
+                DisplayAlert("Resultado", $"{Uno} + {Dos} = {r}", "Cerrar");
+            } 
+            else if (resta.IsChecked)
+            {
+                r = Uno - Dos;
+                DisplayAlert("Resultado", $"{Uno} - {Dos} = {r}", "Cerrar");
+            }
+            else if (division.IsChecked)
+            {
+                r = Uno / Dos;
+                DisplayAlert("Resultado", $"{Uno} / {Dos} = {r}", "Cerrar");
+            }
+            else if (multiplicacion.IsChecked)
+            {
+                r = Uno * Dos;
+                DisplayAlert("Resultado", $"{Uno} * {Dos} = {r}", "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Error", "Valores no definidos correctamente", "Cerrar");
+            }
+            R.Text = r.ToString();
+        }
+    }
+}
